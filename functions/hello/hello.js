f603c3c995db0f9d701990afb22a73695e17fcab
= <=>
2020-10-04 19:57:16 +0500

    fetch  data from function

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,10 +1,10 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || "World";
+    const subject = event.queryStringParameters.name || "here";
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: `Syed Kamal ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  const [state, setstate] = useState({});
+  useEffect(() => {
+    (async () => {
+      let url = await fetch(`.netlify/functions/hello`);
+      let data = await url.json();
+      setstate(data);
+    })();
+  }, []);
+  return <div className="App">hello world {state.message}</div>;
 }
 
 export default App;
